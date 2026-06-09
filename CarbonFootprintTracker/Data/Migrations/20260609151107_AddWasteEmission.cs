d04b62a2179dad90a0f99969222905e87577940b
TarshikaMendis <tarshikawickramanayaka@gmail.com>
2026-06-09 20:56:55 +0530

    Do some changes

---
+++ CarbonFootprintTracker/Data/Migrations/20260609151107_AddWasteEmission.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarbonFootprintTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWasteEmission : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "WasteEmission",
+                table: "CarbonRecords",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WasteEmission",
+                table: "CarbonRecords");
+        }
+    }
+}
